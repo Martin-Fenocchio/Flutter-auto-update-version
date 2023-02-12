c06782a1ea1d320f1f0d1cc90ee929246e86bd18
Martín Fenocchio <fenomartin6@gmail.com>
2023-02-12 16:31:41 -0300

    feature: auto update version

---
+++ index.js
@@ -0,0 +1,17 @@
+const YAML = require("yaml");
+const fs = require("fs");
+
+const filePath =
+  "/Users/martinfenocchio/Documents/GitHub/flutter-gorilla-hash/pubspec.yaml";
+const file = YAML.parse(fs.readFileSync(filePath, "utf8"));
+
+const appVersionStringify = file.version;
+
+const lastNumberIndex = appVersionStringify.indexOf("+");
+const appVersionNow = appVersionStringify.substring(4, lastNumberIndex);
+
+const newVersion = parseInt(appVersionNow) + 1;
+
+file.version = appVersionStringify.replace(appVersionNow, newVersion);
+
+fs.writeFile(filePath, YAML.stringify(file), (err) => {});
