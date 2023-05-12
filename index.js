2cd781fed9749bb4f23c5751d689f62263d07124
Martín Fenocchio <fenomartin6@gmail.com>
2023-05-12 16:54:30 -0300

    add notifications and delete .env

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const YAML = require("yaml");
 const fs = require("fs");
+const notifier = require("node-notifier");
 require("dotenv").config();
 
 const filePath = process.env.pubspecPath;
+
 const file = YAML.parse(fs.readFileSync(filePath, "utf8"));
 
 const appVersionStringify = file.version;
 
 const lastNumberIndex = appVersionStringify.indexOf("+");
-const appVersionNow = appVersionStringify.substring(4, lastNumberIndex);
+const appVersionNow = appVersionStringify
+  .substring(4, lastNumberIndex)
+  .replaceAll(".", "");
 
 const newVersion = parseInt(appVersionNow) + 1;
 
 file.version = appVersionStringify.replace(appVersionNow, newVersion);
 
 fs.writeFile(filePath, YAML.stringify(file), (err) => {});
+
+notifier.notify(
+  {
+    title: "Nueva version!",
+    message: "La nueva version es la " + newVersion.toString(),
+  },
+  (err, response, metadata) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log("Notificación enviada:", response, metadata);
+  }
+);
